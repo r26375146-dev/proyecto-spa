ccb31fc8045b3113a5b7596dad3c3402ce79d1ff
r26375146-dev <r26375146@gmail.com>
2026-06-08 22:39:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import urllib.request
 import urllib.parse
 import os
 
+# Corrección 1: Se necesitan guiones bajos dobles en name
 app = Flask(__name__)
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
@@ -27,5 +28,8 @@ def inicio():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
- if __name__ == '__main__':
-       app.run(debug=True)
+
+# Corrección 2: Guiones bajos en name y main
+# Corrección 3: Asegúrate de que el if no tenga espacios a la izquierda
+if __name__ == '__main__':
+    app.run(debug=True)
